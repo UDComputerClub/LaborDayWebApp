5bed8b15a0dfa38b454fcc8c644cfa94f17574cb
Brian McCutchon <mccutchon.brian@gmail.com>
2016-09-03 11:10:23 -0500

    Add REST mapping skeleton

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/rest/video', (req, res) => {
+    // Use req.body to get the POST params, but we need something else to parse
+    // the request body. See http://expressjs.com/en/api.html#req.body
+});
+
 app.listen(3000);
