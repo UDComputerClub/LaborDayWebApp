b4001f56bb339b40af871c3a43ea16fbe2778f22
fyodr <tedmoriniv@gmail.com>
2016-09-03 17:23:48 -0500

    some progress towards making images visible after they are selected - not completed

--- public/clientscript.js
+++ public/clientscript.js
@@ -5,11 +5,13 @@ clientScript.controller('clientController', function($scope) {
 
     // Staged Images - eventually stores the image data
     $scope.stages = {1 : null, 2 : null};
+    $scope.stagestatus = {1 : false, 2 : false};
     
     // handles a new file selection of an image for stage 1 or stage 2
     $scope.selectStage = function(stage) {
-        // TODO
-        // must display the 
+        //if (TODO error check) {
+            $scope.stagestatus[stage] = true;
+        //}
     };
     
     // crops a selected file based on its preview
@@ -21,6 +23,27 @@ clientScript.controller('clientController', function($scope) {
     $scope.submitStages = function() {
         // TODO
     }
+    
+    // code directly from Endy Tjahjono
+    // http://stackoverflow.com/questions/17063000/ng-model-for-input-type-file
+    $scope.directive("fileread", [function () {
+        return {
+            $scope: {
+                fileread: "="
+            },
+            link: function ($scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+                        $scope.$apply(function () {
+                            scope.fileread = loadEvent.target.result;
+                        });
+                    }
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        }
+    }]);
     
     /*
     // show the image uploaded 
