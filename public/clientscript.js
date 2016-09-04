76274fbbdc1f49a6943f3e56609d1decf82b148f
fyodr <tedmoriniv@gmail.com>
2016-09-04 12:28:36 -0500

    sending up what I have

--- public/clientscript.js
+++ public/clientscript.js
@@ -1,16 +1,59 @@
 
 
 var clientScript = angular.module('clientScript', []);
+
+
+// code directly from danial
+// http://stackoverflow.com/questions/13963022/angularjs-how-to-implement-a-simple-file-upload-with-multipart-form
+var MyCtrl = [ '$scope', 'Upload', function($scope, Upload) {
+  $scope.onFileSelect = function($files) {
+    console.log("here");
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var $file = $files[i];
+      console.log("here");
+      /*Upload.upload({
+        url: 'my/upload/url',
+        file: $file,
+        progress: function(e){}
+      }).then(function(data, status, headers, config) {
+        // file is uploaded successfully
+        console.log(data);
+      }); */
+    }
+  }
+}];
+
 clientScript.controller('clientController', function($scope) {
 
     // Staged Images - eventually stores the image data
-    $scope.stages = {1 : null, 2 : null};
-    $scope.stagestatus = {1 : false, 2 : false};
+    $scope.stage1 = null;
+    $scope.stage2 = null;
+    $scope.stage1Shown = false;
+    $scope.stage2Shown = false;
+    $
     
     // handles a new file selection of an image for stage 1 or stage 2
-    $scope.selectStage = function(stage) {
+    $scope.selectStage = function(files) {//, stage) {
         //if (TODO error check) {
-            $scope.stagestatus[stage] = true;
+            console.log("here");
+            stage=1;
+            if (stage == 1) {
+                $scope.stage1Shown = true;
+            }
+            if (stage == 2) {
+                $scope.stage2Shown = true;
+            }
+            var fd = new FormData();
+            //Take the first selected file
+            fd.append("stage"+stage.toString(), files[0]);
+
+            /*uploadUrl, fd, {
+                withCredentials: true,
+                headers: {'Content-Type': undefined },
+                transformRequest: angular.identity
+            }).success( ...all right!... ).error( ..damn!... );*/
+            
         //}
     };
     
@@ -24,26 +67,14 @@ clientScript.controller('clientController', function($scope) {
         // TODO
     }
     
-    // code directly from Endy Tjahjono
+    // tried code from Fabio
+    // http://stackoverflow.com/questions/13963022/angularjs-how-to-implement-a-simple-file-upload-with-multipart-form
+    
+    // tried code directly from pawel
+    // http://stackoverflow.com/questions/19986178/displaying-an-image-after-uploading-in-angular-js
+    
+    // tried code directly from Endy Tjahjono
     // http://stackoverflow.com/questions/17063000/ng-model-for-input-type-file
-    $scope.directive("fileread", [function () {
-        return {
-            $scope: {
-                fileread: "="
-            },
-            link: function ($scope, element, attributes) {
-                element.bind("change", function (changeEvent) {
-                    var reader = new FileReader();
-                    reader.onload = function (loadEvent) {
-                        $scope.$apply(function () {
-                            scope.fileread = loadEvent.target.result;
-                        });
-                    }
-                    reader.readAsDataURL(changeEvent.target.files[0]);
-                });
-            }
-        }
-    }]);
     
     /*
     // show the image uploaded 
