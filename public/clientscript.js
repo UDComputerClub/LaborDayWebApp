7f057744901d734d2ae39fee07f47b1a6107444c
Brian McCutchon <mccutchon.brian@gmail.com>
2016-09-04 23:55:05 -0500

    Clean up whitespace
    Remove trailing whitespace. Fix indentation. Use spaces.

--- public/clientscript.js
+++ public/clientscript.js
@@ -15,12 +15,12 @@ clientScript.controller('clientController', function($scope, Upload) {
 
     $scope.evolutionStyle = $scope.evolveStyles[0];
 
-	var animateOn = false;
-	var startTime;
-	var canvasWidth = 300;
-	var canvasHeight = 300;
-	var image1 = new Image();
-	var image2 = new Image();
+    var animateOn = false;
+    var startTime;
+    var canvasWidth = 300;
+    var canvasHeight = 300;
+    var image1 = new Image();
+    var image2 = new Image();
 
     // Gets the number of milliseconds for which to display each image when
     // oscillating between images in certain evolution styles.
@@ -30,7 +30,7 @@ clientScript.controller('clientController', function($scope, Upload) {
 
     // code from danial
     // http://stackoverflow.com/questions/13963022/angularjs-how-to-implement-a-simple-file-upload-with-multipart-form
-    
+
     // handles a new file selection of an image for stage 1 or stage 2
     $scope.onFileSelect = function(file) {
         console.log("here");
@@ -41,49 +41,49 @@ clientScript.controller('clientController', function($scope, Upload) {
         console.log("selectStage");
         $scope.stageImage[stage] = getAsDataURL(file);
         $scope.stageShown[stage] = true;
-        var fd = new FormData(); 
+        var fd = new FormData();
         //Take the first selected file
         fd.append("stage"+stage.toString(), files[0]);
     };
-    
+
     // crops a selected file based on its preview
     $scope.cropStage = function(stage) {
         console.log("test");
         // TODO
     };
-    
+
     // submit the stages selected
     $scope.submitStages = function() {
         // TODO
     };
-	
-	$scope.initAnimation = function(){
-		startTime = new Date();
-		Upload.base64DataUrl([$scope.stages[0].image, $scope.stages[1].image])
-			.then(function (urls) {
-				image1.src = urls[0];
-				image2.src = urls[1];
-				animateOn = true;
-			});
-	};
 
-	$scope.animate = function(ctx) {
-	  if(animateOn){
-		ctx.globalCompositeOperation = 'destination-over';
-	  ctx.clearRect(0,0,canvasWidth,canvasHeight); // clear canvas
+    $scope.initAnimation = function(){
+        startTime = new Date();
+        Upload.base64DataUrl([$scope.stages[0].image, $scope.stages[1].image])
+            .then(function (urls) {
+                image1.src = urls[0];
+                image2.src = urls[1];
+                animateOn = true;
+            });
+    };
 
-	  ctx.fillStyle = 'black';
-	  ctx.strokeStyle = 'black';
-	  ctx.save();
-	  //ctx.translate(150,150);
+    $scope.animate = function(ctx) {
+        if(animateOn){
+            ctx.globalCompositeOperation = 'destination-over';
+            ctx.clearRect(0,0,canvasWidth,canvasHeight); // clear canvas
 
-	  var elapsed = new Date() - startTime;
-	  var isStage1 = (elapsed%(2*getMs(elapsed)))<getMs(elapsed);
+            ctx.fillStyle = 'black';
+            ctx.strokeStyle = 'black';
+            ctx.save();
+            //ctx.translate(150,150);
 
-	  ctx.drawImage(isStage1 ? image1 : image2,0,0,canvasWidth,canvasHeight);
-	}
-	};
-    
+            var elapsed = new Date() - startTime;
+            var isStage1 = (elapsed%(2*getMs(elapsed)))<getMs(elapsed);
+
+            ctx.drawImage(isStage1 ? image1 : image2,0,0,canvasWidth,canvasHeight);
+        }
+    };
+
 })
 .directive('ldwRender', function() {
     return {
